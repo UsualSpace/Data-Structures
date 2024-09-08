2c576295370039ef8f70204bb17f0122d3904e60
Abdurrahman Alyajouri <70238647+UsualSpace@users.noreply.github.com>
2024-09-08 01:56:42 -0700

    Update LRU_index_cache.cpp

--- LRU_index_cache.cpp
+++ LRU_index_cache.cpp
@@ -6,18 +6,29 @@
   Description: A slightly modified version of the LRU cache concept, in which instead of being able to insert a key and value pair, 
                you can only enter in a key, and the data structure will assign to this key an available index in an array or other container.
                As the cache fills up, it assigns sequential indices to incoming keys. When all indices have been taken up or used (dependent on the capacity variable),
-               The structure begins to follow the rules of Least Recently Used (LRU) principle, and evicts the least recently used/accessed key from the cache, and recycles its index
+               The structure begins to follow the rules of the "least recently used" (LRU) principle, and evicts the least recently used/accessed key from the cache, and recycles its index
                to pair to the new incoming key. 
                
-               TLDR: This structure auto assigns to a key the integer index of the next available spot in some integer-indexed fixed-size container, based on LRU policies.
-               
+               TLDR: This structure auto assigns to a key the integer index of the next available spot in some integer-indexed fixed-size container, based on LRU policies. 
+                    
+               Note: This data structure simply maps keys to indices for a theoretical container of the same capacity as defined for the structure below. The retrieved indices can be used to access elements
+                     in some container of the same size.
+                    
                One use case of this structure is in one of my projects involving chunk based terrain rendering. The problem was that each chunk in the terrain required a unique texture, 
                and all the textures were stored in a limited array of textures. Because the only unique and known aspect of a chunk was its position in the world, I used said chunk positions as keys and paired
                them to indices in the array of textures, so that a texture could be easily corresponded to a chunk based on it's position. Given a list of chunks that need to be rendered, for each chunk, I would check if
                its world position was already a key in the cache. If the key does exist already, nothing happens. If not, I insert said chunk position into the cache, and it is paired with 
                the next available index into the array of textures. A texture is then generated at this index.
                When actually rendering, a list of chunks to be rendered would be given to the GPU, as well as a parallel list of indices into the texture array for a given chunk in the chunk list, 
-               that was obtained by using the functions of the below data structure. 
+               that was obtained by using the functions of the below data structure. It allowed easy access and correspondence between terrain chunks and textures within a texture array.
+
+               Function roles:
+                   bool Exists(const K& key) const : Returns true if a key is already in the cache, false otherwise.
+                   int Get(const K& key) : Returns the integer index in some fixed-size container assigned to the given key value. Returns -1 if the given key was not in the cache.
+                   int GetUsed() const : Returns the number of used up indices/spots in the cache.
+                   int GetCapacity() const : Returns the capacity of the cache.
+                   void Put(const K& key): For the given key, inserts it into the cache and assigns it the next available index decided by said cache.
+                   void PrintCacheState() const : Prints a list of all the key-index pairs in the cache at any given moment.
 */
 
 template<typename K>
@@ -30,7 +41,7 @@ class LRUIndexCache {
             this->capacity = capacity;
             this->used = 0;
         }
-        
+
         bool Exists(const K& key) const {
             return kvmap.find(key) != kvmap.end();
         }
